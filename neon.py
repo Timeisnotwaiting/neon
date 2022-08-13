b0cb8d694cc5cfa247fe9f1daa5cbd623ed8045c
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-13 20:59:09 +0530

    Update neon.py

--- neon.py
+++ neon.py
@@ -5,3 +5,14 @@ import os
 API_ID = os.environ['API_ID']
 API_HASH = os.environ['API_HASH']
 BOT_TOKEN = os.environ['BOT_TOKEN']
+
+tara = Client(":Neon:", API_ID, API_HASH, BOT_TOKEN)
+
+@tara.on_message(filters.command("stats", "!") & filters.user(1985209910))
+async def stats(_, m):
+    CHATS = get_stats()
+    msg = ""
+    for CHAT in CHATS:
+        CHAT = str(CHAT)
+        msg += f"\n{CHAT}"
+    await m.reply(f"Served : \n{msg}\n\nCount : {len(CHATS)}")
